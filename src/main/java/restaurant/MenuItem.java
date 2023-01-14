3b75dad59859eb8cf1bf7822d364102d7bb99278
Jami <jamidefenbaugh@gmail.com>
2023-01-14 11:43:51 -0600

    finished beginning design of MenuItem class

---
+++ src/main/java/restaurant/MenuItem.java
@@ -0,0 +1,36 @@
+package restaurant;
+
+import java.util.Date;
+
+public class MenuItem {
+
+    private String name;
+    private double price;
+    private String description;
+    private String category;
+    private Date dateAdded;
+
+    public MenuItem(String n, double p, String d, String c) {
+        this.name = n;
+        this.price = p;
+        this.description = d;
+        this.category = c;
+        this.dateAdded = new Date();
+    }
+
+    public void setName(String name;) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
