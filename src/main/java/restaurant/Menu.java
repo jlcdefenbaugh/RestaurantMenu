9b45cc1b7c806cd5a45be0de79df10c8c83b2e9f
Jami <jamidefenbaugh@gmail.com>
2023-01-14 11:45:50 -0600

    finished beginning design of Menu class

---
+++ src/main/java/restaurant/Menu.java
@@ -0,0 +1,11 @@
+package restaurant;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Menu {
+
+        private ArrayList<MenuItem> menuItems = new ArrayList<>();
+        private Date lastUpdated;
+
+}
